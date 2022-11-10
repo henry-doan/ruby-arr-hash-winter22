008b5c3a4699c31257c0de9dc7c7b715f221c792
Henry Doan <henrydoan96@gmail.com>
2022-11-09 19:36:37 -0700

    array start

---
+++ notes.rb
@@ -0,0 +1,45 @@
+# Array 
+  # - collection of data items 
+  # - ref by position 
+  # - any datatypes 
+  # - order does matter 
+
+  # arr = Array.new() 
+  # arr = []
+  # arr = ['bob', 'drake', 'josh']
+  # arr = ['bob', 123, true, []]
+
+  # - index - position of the item 
+  # - 0 based, start from 0 
+  #  0 .     1 .     2
+  # ['bob', 'drake', 'josh']
+  # letters = ['a', 'b', 'c']
+  # nameofarr[index] - grab item by index
+  # puts letters[1]
+  # puts letters[0]
+  # puts letters[2]
+  # puts letters.first
+  # puts letters.last
+  # p letters[5]
+  # puts letters[5]
+  # puts letters[-1]
+            # letter letter letter letter letter
+        # i   0   1    2    3     4
+  # letters = ['a', 'b', 'c', 'd', 'e']
+  # puts letters[0]
+  # puts letters[1]
+  # puts letters[2]
+  # puts letters[3]
+  # puts letters[4]
+  
+  # iterator - goes through each value of the arr 
+  # letters.each do |letter|
+  #   puts letter
+  # end
+                              # each item, index 
+  # letters.each_with_index do |letter, i|
+  #   puts "#{i}. #{letter}"
+  #   puts "#{i + 1}. #{letter}"
+  # end
+
+
